94dca96f795851eae279abdb05f1869a50b0563c
shab-yalda <121182845+shab-yalda@users.noreply.github.com>
2022-12-21 20:26:23 +0330

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+
+var 
+  $player = $(".audio-ctrl"),
+  $transcript = $(".audio-transcript");
+
+  $(".audio-ctrl").each(function(){
+
+    $(this).attr("aria-pressed","false");
+
+    $(this).click(function(e){
+      e.preventDefault();
+
+      $player.attr("aria-pressed","false");
+
+      if($(this).hasClass("play")){
+        $(this).attr("aria-pressed","true");
+        // 2 sec delay to allow screen reader
+        // to read button state
+        setTimeout(function(){
+          $("#player")[0].play();
+        },1000);
+      }
+      if($(this).hasClass("pause")){
+        $("#player")[0].pause();
+        $(this).attr("aria-pressed","true");
+      }
+      if($(this).hasClass("read")){
+        $transcript.removeClass("visually-hidden").focus();
+      }
+    });
+  });
+  
+  window.addEventListener("load",function() {
+    var loadScreen=document.querySelector(".preloader-container");
+    document.body.removeChild(loadScreen);
+  })
+  
